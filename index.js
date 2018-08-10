14435d267d2561248098dfef701c0c3f594dacea
DFA_PRIMARY\rliu1 <rui.liu@dimensional.com>
2018-08-10 14:36:07 -0500

    init commit - Rui

---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require("express");
+
+const app = express();
+
+app.listen(3002, (err) => {
+if (err) {
+  return console.log("Error", err);
+}
+console.log("Web server is now living in apartment 3002");
+});
