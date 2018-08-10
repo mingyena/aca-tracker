7dbee9ce05e1d188b2ed65ac52e1d6acd40e433c
Rui Liu <mingyena@gmail.com>
2018-08-10 14:46:43 -0500

    nodemon added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 
 const app = express();
+const theParser = bodyParser.json();
 
+app.get("/secret",function(request,response,next){
+    response.send("Secret Information");
+   });
+
+   
 app.listen(3002, (err) => {
 if (err) {
   return console.log("Error", err);
